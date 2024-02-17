ea3e3a161eeb9f4458ab57d717c31d6ba86d5b70
davidbte06 <davidbte06@gmail.com>
2024-02-17 11:28:49 -0600

    Prisma install, Footer and Navbar, Login and Sign up Form

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SignInForm from "@/components/auth/LoginForm";
+
+const Login = () => {
+  return <>
+    <SignInForm />
+  </>
+};
+
+export default Login;
---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SignUpSection from "@/components/auth/RegisterForm";
+
+const Register = () => {
+  return <>
+  <SignUpSection />
+  </>
+};
+
+export default Register;
